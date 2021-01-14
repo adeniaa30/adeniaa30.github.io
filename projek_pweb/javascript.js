f41048406730d78d0c73e9112cbe6289c42f439b
shintiaanggi12 <72627338+shintiaanggi12@users.noreply.github.com>
2021-01-15 01:15:39 +0700

    Create javascript.js

---
+++ projek_pweb/javascript.js
@@ -0,0 +1,8 @@
+function cetak() {
+    "use strict";
+    alert("Thank You for Your Suggestions! Don't forget to be Happy!:)<3");
+        }
+function email() {
+    "use strict";
+    alert("You have a Subscribe Us!<3");
+        }
